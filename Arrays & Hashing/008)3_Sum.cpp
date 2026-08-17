83fdf46138a2b5fc24a44ddd88fcefa5330d7fdb
maruthupriyanka27 <maruthupriyanka27@gmail.com>
2026-08-17 07:45:54 +0530

    3_Sum solved

---
+++ Arrays & Hashing/008)3_Sum.cpp
@@ -0,0 +1,53 @@
+// 3Sum: Sort the array, fix one element, and use two pointers to find the other two.
+// Time: O(n^2) | Space: O(1) excluding the output
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+
+        vector<vector<int>> ans;
+
+        sort(nums.begin(), nums.end());
+
+        for(int i = 0; i < nums.size() - 2; i++)
+        {
+            if(i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int left = i + 1;
+            int right = nums.size() - 1;
+
+            while(left < right)
+            {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if(sum < 0)
+                {
+                    left++;
+                }
+                else if(sum > 0)
+                {
+                    right--;
+                }
+                else
+                {
+                    ans.push_back({nums[i], nums[left], nums[right]});
+
+                    left++;
+                    right--;
+
+                    while(left < right && nums[left] == nums[left - 1])
+                    {
+                        left++;
+                    }
+
+                    while(left < right && nums[right] == nums[right + 1])
+                    {
+                        right--;
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+};
